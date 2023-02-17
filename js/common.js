eadf703ff6288da0d2bce64ca158ba025da04bb5
Han ahreum <arhan@enliple.com>
2023-02-17 10:25:08 +0900

    test

--- js/common.js
+++ js/common.js
@@ -181,9 +181,9 @@ $(function () {
         var originalSize = jQuery(window).height();
         jQuery(window).resize(function(){
             if(jQuery(window).height() != originalSize) {
-                $('.uni-mob .bottom-menu,.uni-mob .btn-fixed-area,.uni-mob .station-sub .btn-area').addClass('hide');  
+                $('.uni-mob .bottom-menu,.uni-mob .btn-fixed-area,.uni-mob.station-sub .btn-area,.uni-mob.qna-write .sub-menu').addClass('hide');  
             }else{
-                $('.bottom-menu,.btn-fixed-area,.station-sub .btn-area').removeClass('hide');  
+                $('.bottom-menu,.btn-fixed-area,.station-sub .btn-area,.qna-write .sub-menu').removeClass('hide');  
             }
         });
     }
