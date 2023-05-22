de3fcb7bf8f66a716f6603f51960aaf51e34032e
Han ahreum <arhan@enliple.com>
2023-05-22 18:55:44 +0900

    ai홈페이지test

--- js/common.js
+++ js/common.js
@@ -1,184 +1,209 @@
-$(function(){  
-    //해상도 클래스
-    function responsiveCheck(){
-        if($(window).width() <= 768){
-            $('body').addClass('mode_mob');
-            $('.gnb_menu').off('mouseover');
-            $('header .center').off('mouseleave');
-        }else{
-            $('body').removeClass('mode_mob');
-            $('.gnb_menu').on('mouseover',function(){
-                $('header .center').addClass('on');
-            });
-            $('header .center').on('mouseleave',function(){
-                $('header .center').removeClass('on');
-            });
-        }
-    }
-    responsiveCheck();
-
-    //윈도우 리사이즈 이벤트     
-    const windowWidth= $(window).width(); 
-    const windowHeight= $(window).height();    
-    $(window).resize(function(){
-        responsiveCheck();
-        if($(window).width() > 768){
-            //모바일 해제시 모바일메뉴 remove on
-            $('header .center,.mobMenuClose').removeClass('on');
-            scrollAuto(scrollHeight);
-        }
-        //모바일 fixed bottom 요소 inherit (디바이스 키패드on시 화면 리사이즈)
-        if($('body').hasClass('device_mob')){
-            if($(window).height() != windowHeight){
-                $('.bottomFixedWrap').addClass('hide'); 
-            }else{
-                $('.bottomFixedWrap').removeClass('hide'); 
-            }
-        }
-    });
-
-    //헤더 메뉴
-    $('.mobMenuOpen').click(function(){
-        $('header .center').addClass('on');
-        $('.mobMenuClose').addClass('on');
-        scrollDim();
-    });
-    $('.gnb_menu>li>span').click(function(){
-        $(this).parent().siblings().removeClass('on');
-        $(this).parent().toggleClass('on');
-    });
-    $('.mobMenuClose').click(function(){
-        $(this).removeClass('on');
-        $('header .center').removeClass('on');
-        scrollAuto(scrollHeight);
-    });
-
-    //스크롤 통제
+$(document).ready(function(){
+    //스크롤
+    let pageWrap = document.querySelector('.pageWrap');  
+    let page = document.querySelectorAll('.page'); 
+    let pageLength = page.length;
+    let current = 0;
+    let windowHeight = window.innerHeight;      
+    let footerHeight = $('.page.footer').innerHeight();
     let scrollHeight = 0;
-    function scrollDim() {
-        scrollHeight = $(window).scrollTop();
-        $("body").css({"position": "fixed","top": -scrollHeight});
-    } 
-    function scrollAuto(scrollHeight) {
-        $("body").css({"position": "relative","top": 0 });
-        $(window).scrollTop(scrollHeight);
-    }
-    
-    //탑스크롤버튼
-    $('.topBtn').on('click', function () {
-        window.scroll({
-            top: 0,
-            behavior: 'smooth'
+    let animation = true;    
+            
+    function ani(){
+        animation = false;
+        pageWrap.addEventListener('transitionend',function(){
+            animation = true;
         });
-    });
-        
-    const contentHeight= $('.content').height();   
-    function stView(){
-        if( contentHeight > 1080){
-            if($(window).scrollTop() > 0){
-                $('.topBtn').css('display','block');
-            }else{
-                $('.topBtn').css('display','none');
+    }
+    function wheel(){
+        if(current == pageLength-1){
+            scrollHeight = windowHeight*(current-1) + footerHeight; 
+        }else{
+            scrollHeight = windowHeight*current; 
+        }
+        $(".pageWrap").css('transform','translateY(-'+scrollHeight+'px)');
+        $(".page").removeClass('now');
+        $(".page").eq(current).addClass('now on');
+    }     
+    window.addEventListener('resize',function(){
+        windowHeight = window.innerHeight;   
+        footerHeight = $('.page.footer').innerHeight();
+        //windowHeight =  $(".page").eq(current).innerHeight();  
+        wheel();  
+    })
+    function up(){   
+        if( current > 0 && $(".page").eq(current).children('.scrollBox').scrollTop() == 0 && animation == true){
+            current--; 
+            ani();
+            wheel();    
+        }   
+    }
+    function down(){
+        if( current < pageLength-1 && animation == true){
+            if($(".page").eq(current).children('.scrollBox').scrollTop() + $(".page").eq(current).children('.scrollBox').innerHeight() >= $(".page").eq(current).children('.scrollBox').prop('scrollHeight')){
+                current++; 
+                ani();
+                wheel();
             }
         }
-    }
-    stView();
-    $(window).scroll(function(){
-        stView();
-    });
-
-
-    //팝업
-    $('.popOpenBtn').on('click',function(){
-        let popNow = $('#' + $(this).data("popname") + '');
-        let clickContent = $(this);
-        popNow.addClass('on');
-        scrollDim();
-        if (popNow.hasClass('typeTimer')) {
-            setTimeout(function () {
-                popNow.find('.popDim').trigger("click");
-            }, 5000)
+    }        
+    window.addEventListener("wheel",function(e){               
+        if(e.deltaY<0){
+            up(); 
+        }else{ 
+            down();
         }
     });
-    $('.popCloseBtn, .popDim').on('click',function(){
-        $(this).parents('.popLayer').removeClass('on').removeAttr('tabindex');
-        scrollAuto(scrollHeight);
-        tabAuto();
+    $('.topBtn').on('click',function(){
+        current = 0;
+        wheel();
     });
 
+    var UserAgent = navigator.userAgent;
+    if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null){//모바일
+        console.log('mobile js');
+        var mobClass = document.querySelector('.container');
+        mobClass.classList.add('is-mob');
+
+        $(window).on('touchstart',function(e){
+            e.preventDefault();
+            startY = e.originalEvent.changedTouches[0].screenY;
+        });
+        $(window).on('touchend',function(e){
+            e.preventDefault();
+            endY=e.originalEvent.changedTouches[0].screenY;  
+            touchY = endY - startY;
+            if( touchY <= 0){   
+                if( current < pageLength-1 && animation == true){
+                    down();
+                }
+            }else{
+                up(); 
+            }
+            
+        });
+            
+    }else{ //PC 
+        console.log('pc js');        
+    }
     
-    //탭박스
-    $('.tabList li').on('click',function () {
+
+    let percentage = 0;
+    function progressBar(cont){
+        percentage = Math.floor((cont.scrollTop() /
+            (cont.prop("scrollHeight") - cont.prop("clientHeight"))
+            ) * 100);     
+        cont.find('i').css('height',percentage+'%');
+    }
+    $('.progressBarBox').on('scroll',function(){
+        progressBar($(this));
+    });
+    
+
+    let fadeLi = $('.animateFadeList>li');
+    let fadeLength = fadeLi.length;
+    let fadeCont = fadeLi.find('.fadeCont');
+    let fadeSection = Math.floor(($('.animateFadeWrap').prop("clientHeight"))/fadeLength);
+    let fadeIndex = 1;
+    let fadeScroll = 0;
+    let fadeArray = new Array();
+    for( var i = 0; i <= fadeLength ; i++){
+        fadeArray.push(fadeSection * i);
+    }
+    function fadeWheelDown(eq){
+        fadeCont.removeClass('wheelup active next prev');
+        fadeLi.removeClass('now start');
+        fadeLi.eq(eq).addClass('now');
+        fadeLi.eq(eq).prev().find('.fadeCont').addClass('wheeldown prev');
+        fadeLi.eq(eq).find('.fadeCont').addClass('wheeldown active on'); 
+        if( fadeLi.eq(eq).find('input[type="checkbox"]').prop("checked") == false){
+            fadeLi.eq(eq).find('input[type="checkbox"]').prop('checked',true).trigger("change");            
+        }
+    }
+    function fadeWheelUp(eq){
+        fadeCont.removeClass('wheeldown active next prev');
+        fadeLi.removeClass('now');
+        fadeLi.eq(eq).prev().addClass('now');
+        fadeLi.eq(eq).find('.fadeCont').addClass('wheelup active');       
+        fadeLi.eq(eq).prev().find('.fadeCont').addClass('wheelup next');
+    }
+    $('.animateFadeBox').on('scroll',function(){       
+        fadeScrollNow = $(this).scrollTop();           
+        if( fadeScrollNow > fadeArray[fadeIndex-1] && fadeScrollNow <= fadeArray[fadeIndex]){
+            // console.log(fadeIndex);               
+        }else{
+            if(fadeScrollNow > fadeScroll){
+                fadeIndex += 1;
+                fadeWheelDown(fadeIndex-1);
+            }else{
+                if(fadeIndex > 1){
+                    fadeIndex -= 1;
+                    fadeWheelUp(fadeIndex);
+                }
+            }
+        }
+        fadeScroll = fadeScrollNow;
+    });
+
+
+    let circleLength = $('.animateCircleList>li').length;
+    let circleIndex = 0;
+    let circleScroll = 0;
+    let circleScrollNow;
+    let circleHeight = $('.animateCircleList').data("height");
+    var delay = 300;
+    var timer = null;
+    function circleFunction(circleScrollNow){        
+        if(circleScrollNow > circleScroll){
+            if(circleIndex < circleLength){
+                circleIndex += 1;
+            }
+        }else{
+            if(circleIndex > 0){
+                circleIndex -= 1;
+            }
+        }    
+        $('.aniCircle').removeClass('circle00 circle01 circle02 circle03 circle04');
+        $('.aniCircle').addClass('circle0'+circleIndex+'');
+        $('.animateCircleList>li').eq(circleIndex-1).addClass('on');
+        $('.animateCircleScroll').css('transform','translateY(-'+circleHeight*circleIndex+'rem)');
+        circleScroll = circleScrollNow;
+        console.log(circleIndex)  ;
+    }
+    $('.animateCircleBox').on('scroll',function(){    
+        circleScrollNow = $(this).scrollTop();            
+        clearTimeout(timer);
+        timer = setTimeout(function(){
+          circleFunction(circleScrollNow);
+        }, delay);
+    });
+
+
+
+
+    $('.mobMenuBtn').on('click',function(){
+        $(this).toggleClass('on');
+    });
+    
+    $('.tabList li button').on('click',function () {
         let tabWrap = $(this).parents('.tabWrap');
         let tabLi = tabWrap.find('.tabList li');
         let tabBox = tabWrap.find('.tabBox');
-        let tabIndex = $(this).index();
+        let tabIndex = $(this).parent().index();
         tabLi.removeClass('on');
         tabBox.removeClass('on');
         tabLi.eq(tabIndex).addClass('on');
         tabBox.eq(tabIndex).addClass('on');
     });
-    $('.dataTabList li button').on('click',function () {
-        $(this).parent().siblings().removeClass('on');
-        $(this).parent().addClass('on');
-    });
-    $('.openListWrap li > a[href="#"]').on('click',function (e) {
-        //$(this).parent().siblings().removeClass('on');
-        e.preventDefault();
-        $(this).parent().toggleClass('on');
-    });
-
-    //정보입력 버튼 on event
-    $('.certificationFrom>ul>li:first-child button').on('click',function () {
-        $(this).siblings().removeClass('on');
-        $(this).addClass('on');
-    });
-
-    //이중 체크박스     
-    $(".checkWrap input[type='checkbox']").on("change", function () {
-        let chkWrap = $(this).closest('.checkWrap');
-        let chkAll = chkWrap.find(".chkAll");
-        let chkBox = chkWrap.find(".chk");
-        let chkBoxLength = chkBox.length;
-        let checkedLength = chkWrap.find(".chk:checked").length;
-
-        let topWrap = $(this).parents('.checkWrap');
-        let chkAllTop = topWrap.find('.chkAllTop');
-        let allBox = topWrap.find('input[type="checkbox"]');
-        let allBoxLength = allBox.length - 1;
-        let allCheckedLength = topWrap.find('input[type="checkbox"]:checked').length;
-        let chknum = 0;
-
-        if($(this).hasClass('chkAll')){
-            if($(this).prop("checked") == true){
-                chkBox.prop("checked", true);
-            }else{
-                chkBox.prop("checked", false);
-                chkAllTop.prop("checked", false);
-            }
-        }else if($(this).hasClass('chk')){
-            if(chkBoxLength == checkedLength){
-                chkAll.prop("checked", true);
-            }else{
-                chkAll.prop("checked", false);
-                chkAllTop.prop("checked", false);
-            }                    
-        }else if($(this).hasClass('chkAllTop')){
-            if($(this).prop("checked") == true){
-                allBox.prop("checked", true);
-            }else{
-                allBox.prop("checked", false);
-            }
-        }
-        $(allBox).each(function () {
-            if($(this).prop("checked") == true){
-                chknum++;
-            }
-        });	
-        if(allBoxLength == chknum){
-            chkAllTop.prop("checked", true);
-        }
-    });
     
-
+    $('.popOpenBtn').click(function(){
+        $('.popBack').addClass('on');
+        var thispop = $('#'+$(this).data("popname")+'');
+        thispop.addClass('on');
+    }); 
+    $('.popCloseBtn, .popBack').click(function(){
+        $('.popBack').removeClass('on');
+        $('.popLayer').removeClass('on');
+    });
 });
+ 
